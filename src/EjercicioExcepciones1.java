226020c6686826c88c55526ce5e39a413bb33ee8
Nicole9206 <nic.latfig@gmail.com>
2026-04-18 20:40:58 -0400

    ejercicio Excepciones1

---
+++ src/EjercicioExcepciones1.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class EjercicioExcepciones1 {
+    public static void main(String[] args) {
+        final var arrayAleatorio=RandomUtilities.generarRandomArrayInt(5,10,100, new Random());
+        final var scanner = new Scanner(System.in);
+
+        int indexLeido; //iterador
+
+        do{
+            mostrarArray(arrayAleatorio);
+            indexLeido = ScannerManager.leerInt(scanner, "Ingrese el index deseado");
+            if(indexLeido != -1){
+                mostrarElemento(indexLeido, arrayAleatorio);
+            }
+        }while(indexLeido != -1);
+        scanner.close();
+    }
+    static void mostrarElemento(int i, int[] array){
+        System.out.printf("El elemento con index %s es %d%n%n", i, array[i]);
+    }
+    static void mostrarArray(int[] array){
+        System.out.printf("El array es: %s%n", Arrays.toString(array));
+    }
+}
